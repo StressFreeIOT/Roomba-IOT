b98dbbad37c38afafb5b0ead36f84806a0f80c6c
Bert Van Rossem <mrbertonio@gmail.com>
2018-05-17 12:03:54 +0200

    Update AppInfo.h

--- StressFreeRoomba/AppInfo.h
+++ StressFreeRoomba/AppInfo.h
@@ -1,9 +1,9 @@
 #ifndef APPINFO_H
 #define APPINFO_H
 
-#define APPNAME "SF-Roomba"
+#define APPNAME "StressFreeRoomba"
 
-#define MAJOR_VERSION ""
+#define MAJOR_VERSION "0"
 #define MINOR_VERSION "0"
 #define REVISION_VERSION "1"
 #define VERSION MAJOR_VERSION "." MINOR_VERSION "." REVISION_VERSION
