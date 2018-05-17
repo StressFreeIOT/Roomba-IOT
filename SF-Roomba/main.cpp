ab5b8a1737cbe67a11f779a2146eb3179d66d3b8
Bert Van Rossem <mrbertonio@gmail.com>
2018-05-17 11:17:01 +0200

    Added version print

--- SF-Roomba/main.cpp
+++ SF-Roomba/main.cpp
@@ -1,9 +1,8 @@
-//
-// Created by wolve on 09/05/2018.
-//
 #include <iostream>
+#include <AppInfo.h>
 
 int main(int argc, char *argv[]){
-
+  std::cout << "Firmware version: "<< APPNAME_VERSION << std::endl;
+    
   return 0;
 }
