488724082dc92c2e87e6270105eabcf9caf29151
Bert Van Rossem <mrbertonio@gmail.com>
2018-05-17 12:02:00 +0200

    Changed names

---
+++ StressFreeRoomba/main.cpp
@@ -0,0 +1,17 @@
+/*
+Authors
+@ Wesley Werter
+@ Jens Wolvers
+@ Bert van Rossem
+@ Lars van Lent
+*/
+
+#include <iostream>
+#include <AppInfo.h>
+
+int main(int argc, char *argv[]){
+  std::cout << "Firmware version: "<< APPNAME_VERSION << std::endl;
+  std::cout << "Application has started" << std::endl;
+    
+  return 0;
+}
